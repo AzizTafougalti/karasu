f105b763d2c39b74f28f9b6605054ddb53db4a8c
AzizTafougalti <aziz.walker19@gmail.com>
2018-09-21 12:13:41 +0100

    Commit

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -5,17 +5,19 @@
   <div id='trending'
   class="uk-child-width-1-2@s uk-child-width-1-4@m uk-child-width-1-6@l uk-child-width-1-8@xl uk-text-center"
   uk-grid>
-      <anime-card v-for="anime in trending" :anime="anime" :key="anime.id"></anime-card>
+      <anime-card v-for="anime in animes" :anime="anime" :key="anime.id"></anime-card>
   </div>
   <ul class="uk-pagination">
     <li><a @click="page--"><span class="uk-margin-small-right" uk-pagination-previous></span> Previous</a></li>
-    <li class="uk-margin-auto-left"><a @click="page++">Next <span class="uk-margin-small-left" uk-pagination-next></span></a></li>
+    <li class="uk-margin-auto-left"><a @click="showMore">Next <span class="uk-margin-small-left" uk-pagination-next></span></a></li>
 </ul>
 </div>
 </template>
 <script>
 import AnimeCard from "./AnimeCard.vue";
 import gql from "graphql-tag";
+import { json } from "body-parser";
+const LIMIT = 6;
 
 export default {
   components: {
@@ -26,14 +28,14 @@ export default {
   },
   data() {
     return {
-      animes: []
+      page: 1
     };
   },
   apollo: {
-    trending: {
+    animes: {
       query: gql`
-        {
-          trending {
+        query animes($limit: Int, $page: Int) {
+          animes(limit: $limit, page: $page) {
             id
             title: canonicalTitle
             episodeCount
@@ -42,7 +44,29 @@ export default {
             }
           }
         }
-      `
+      `,
+      variables: {
+        page: 1,
+        limit: LIMIT
+      }
+    }
+  },
+  methods: {
+    showMore() {
+      this.page++;
+      this.$apollo.queries.animes.fetchMore({
+        variables: {
+          page: this.page,
+          limit: LIMIT
+        },
+        updateQuery: (previousResult, { fetchMoreResult }) => {
+          const newAnimes = fetchMoreResult.animes;
+
+          return {
+            animes: [...previousResult.animes, ...newAnimes]
+          };
+        }
+      });
     }
   }
 };
